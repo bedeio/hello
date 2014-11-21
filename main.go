2cf1bfeacf1f477a8956b37b8314e4cc9f78d171
bedeio <thomasjbotelho@gmail.com>
2014-11-21 21:16:48 -0200

    Simple webserver. Check result with curl localhost:8080

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import "net/http"
 
 func main() {
-	fmt.Printf("Yoooo\n")
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write( []byte("hello!\n") ) 
 }
